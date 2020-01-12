e6815b20d60cb03a8c3e4d9639f42abb99034610
farhana5 <59806632+farhana5@users.noreply.github.com>
2020-01-12 15:09:05 -0600

    added radius.java
    first code

---
+++ Radius.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Radius {
+
+	public static void main(String[] args) {
+	double radius;
+	double area;
+	
+	radius =20;
+	
+	area = radius *radius *3.14;
+	System.out.println("This is the area with radius "+area);
+	
+	}
+
+}
